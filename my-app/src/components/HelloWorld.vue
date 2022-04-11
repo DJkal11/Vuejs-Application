fe4f2b54ec154f5f05c7186cf5de3a9bd08dd9e7
Lucian <luciankallee@gmail.com>
2022-04-11 22:32:16 +0200

    work in progress

--- my-app/src/components/HelloWorld.vue
+++ my-app/src/components/HelloWorld.vue
@@ -1,104 +1,27 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div class="Section-1">
+      <div class="info-1">
+        <h1 class="title-1">YOUR HIRING REIMAGINED</h1>
+        <p class="content-1">
+          One-stop hiring platform for every purpose-driven organization
+        </p>
+      </div>
+      <button class="GetStartedbtn">Get Started for Free</button>
+    </div>
+    <div class="Section-2">
+      <div class="overlap-1">what</div>
+      <div class="overlap-2"></div>
+    </div>
+    <div>
+      <h1>who</h1>
+    </div>
+    <div>
+      <h1>what</h1>
+    </div>
+    <div>
+      <h1>why</h1>
+    </div>
   </div>
 </template>
 
@@ -117,18 +40,65 @@ export default class HelloWorld extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.Section-1 {
+  background-image: url("../assets/Teamwork.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 34em;
+  text-align: center;
+  color: white;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.info-1 {
+  padding-top: 7em;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.title-1 {
+  font-size: 4.2em;
+  padding-left: 7em;
+  padding-right: 7em;
+  margin-bottom: 0;
 }
-a {
-  color: #42b983;
+
+.content-1 {
+  font-size: 1.5em;
+}
+
+.GetStartedbtn {
+  background-color: yellow;
+  width: 15em;
+  padding: 0.7em;
+  border: 0;
+  border-radius: 50px;
+  font-size: 1.5em;
+}
+
+.Section-2 {
+  height: 32em;
+  position: relative;
+}
+
+.overlap-1 {
+  position: absolute;
+  background-color: pink;
+  width: 80em;
+  height: 15em;
+  margin-top: -3em;
+  margin-left: 5em;
+  margin-right: 5em;
+  border-radius: 20px;
+  z-index: 2;
+}
+
+.overlap-2 {
+  position: absolute;
+  background-color: aquamarine;
+  width: 84em;
+  height: 12em;
+  margin-top: -1.7em;
+  margin-left: 3em;
+  margin-right: 3em;
+  border-radius: 10px;
+  z-index: 1;
 }
 </style>
